81eab20e428e079febd81df4bc16a6768ee908fc
Jason Costello <jason.costello@synapt.ec>
2022-07-12 22:10:20 +0100

    Implemnted get_value() for state interactions

--- core/game.py
+++ core/game.py
@@ -1,4 +1,17 @@
+from enum import Enum
+
 import numpy as np
+
+
+class Direction(Enum):
+    NORTHWEST = (-1, -1)
+    NORTH = (0, -1)
+    NORTHEAST = (1, -1)
+    EAST = (1, 0)
+    SOUTHEAST = (1, 1)
+    SOUTH = (0, 1)
+    SOUTHWEST = (-1, 1)
+    WEST = (-1, 0)
 
 
 class Board:
@@ -10,7 +23,34 @@ class Board:
     def set_state(self, state_array):
         self.state = np.reshape(
             state_array,
-            newshape=(self.width, self.height)
+            newshape=(self.height, self.width)
         )
+
+    def get_value(self, x, y):
+        return self.state[y][x]
+
+    def get_neighbours(self, x, y):
+        neighbours = []
+        for direction in Direction:
+            neighbour_point = (
+                x + direction.value[0],
+                y + direction.value[1]
+            )
+
+            if not self._check_valid_point(*neighbour_point):
+                neighbours.append(None)
+                continue
+
+            neighbour = self.get_value(*neighbour_point)
+            neighbours.append(neighbour)
+        return neighbours
+
+    def _check_valid_point(self, x, y):
+        if x < 0 or x > (self.width - 1):
+            return False
+        if y < 0 or y > (self.height - 1):
+            return False
+        return True
+
 
 
--- core/test_game.py
+++ core/test_game.py
@@ -4,19 +4,21 @@ import pytest
 
 from core import game
 
+
 @pytest.fixture
 def board():
-    board = game.Board(5, 5)
+    board = game.Board(6, 5)
     return board
+
 
 @pytest.fixture
 def state_array():
     state_array = [
-        0, 0, 0, 0, 0,
-        0, 1, 0, 1, 0,
-        0, 0, 0, 0, 0,
-        0, 1, 0, 1, 0,
-        0, 0, 0, 0, 0
+        0, 0, 0, 0, 0, 0,
+        0, 1, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0,
+        0, 1, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0
     ]
     return state_array
 
@@ -32,9 +34,25 @@ def test_board():
 def test_board_set_state(board, state_array):
     board.set_state(state_array)
 
-    assert board.state[0][0] == 0
-    assert board.state[1][1] == 1
-    assert board.state[1][3] == 1
-    assert board.state[3][1] == 1
-    assert board.state[3][3] == 1
-    assert board.state[4][4] == 0
+    assert board.get_value(0, 0) == 0
+    assert board.get_value(1, 1) == 1
+    assert board.get_value(1, 3) == 1
+    assert board.get_value(3, 1) == 1
+    assert board.get_value(3, 3) == 1
+    assert board.get_value(5, 4) == 0
+
+
+def test_board_get_neighbours(board, state_array):
+    board.set_state(state_array)
+
+    result = board.get_neighbours(0, 0)
+    assert result == [None, None, None, 0, 1, 0, None, None]
+
+    result = board.get_neighbours(1, 1)
+    assert result == [0, 0, 0, 0, 0, 0, 0, 0]
+
+    result = board.get_neighbours(4, 4)
+    assert result == [1, 0, 0, 0, None, None, None, 0]
+
+    result = board.get_neighbours(2, 4)
+    assert result == [1, 0, 1, 0, None, None, None, 0]
